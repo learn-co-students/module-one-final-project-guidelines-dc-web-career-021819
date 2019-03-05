e42a599d37444272f330444e23f161aa63d50634
github username <github email address>
2019-03-05 16:21:42 -0500

    create users table

--- db/migrate/02_create_reviews_table.rb
+++ db/migrate/02_create_reviews_table.rb
@@ -1,12 +1,11 @@
-class CreateReviewsTable < ActiveRecord::Migration[5.0]
+class ReviewTable < ActiveRecord::Migration[5.0]
 
   def change
     create_table :reviews do |t|
       t.string :name
+      t.string :content
       t.integer :score
-      t.string :text
     end
-
   end
 
 end
---
+++ db/migrate/03_create_users_table.rb
@@ -0,0 +1,9 @@
+class UserTable < ActiveRecord::Migration[5.0]
+
+  def change
+    create_table :users do |t|
+      t.string :name
+    end
+  end
+
+end
